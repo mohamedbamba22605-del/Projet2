0fd9f5608e9e5aeb924d95a9c98758f8a9c940f0
AkimM226 <akimbamba17@gmail.com>
2026-08-22 14:15:38 +0100

    m30

--- src/App.tsx
+++ src/App.tsx
@@ -116,8 +116,8 @@ function MainApp() {
         {screen === 'saisie' && <SaisieJ1 onSaved={triggerRefresh} />}
         {screen === 'inscription' && <InscriptionContinue />}
         {screen === 'pointage' && <PointageJ2 onPointed={triggerRefresh} />}
-        {screen === 'inscription_match' && <InscriptionMatchScreen />}
-        {screen === 'pointage_match' && <PointageMatchScreen />}
+        {screen === 'inscription_match' && <InscriptionMatch />}
+        {screen === 'pointage_match' && <PointageMatch />}
         {screen === 'admin' && <Admin />}
       </main>
 
